adcd9685e9281a53f80d8eb5df30decbf96cde41
Javier Gonzalez <javier.e.glz@gmail.com>
2021-09-07 13:17:08 +0200

    remove false from frontend [ch126]: removed

--- src/components/TokenInfoComponent.vue
+++ src/components/TokenInfoComponent.vue
@@ -16,15 +16,44 @@
         <button v-else @click.prevent="showImage=true" class="btn bg-burple-100 mb-4 w-48">Show QR Code</button>
       </transition>
     </div>
-    <div id="coin-data" v-if="tokenStatus === 'SUCCESS'" class="flex justify-between flex-wrap">
-      <div><h3 class="text-xl font-black">Price Per Coin: {{ tokenPrice }} </h3></div>
-      <div><h3 class="text-xl font-black">Total Supply: {{ tokenCap }} </h3></div>
-      <div><h3 class="text-xl font-black">USD locked in <strong>{{tokenInfo.symbol}}</strong>: $ {{ tokenEthBalance * ethPrice }}</h3></div>
-    </div>
-      <div v-else-if="tokenStatus === 'FAIL'">
-        <h3 class="text-xl font-black">Token is not deployed yet. If you just deployed it, check the address on
-          EtherScan</h3>
+    <div id="coin-data" v-if="tokenStatus === 'SUCCESS'">
+      <div class="flex justify-between flex-wrap">
+               <!-- <div><h3 class="text-l"><span class="font-black">${{ tokenPrice }}</span> per token</h3></div> -->
+        <div><h3 class="text-l"><span class="font-black">{{ tokenCap }}</span> in circulation</h3></div>
+        <div><h3 class="text-l"><span class="font-black">${{ tokenEthBalance * ethPrice }}</span> USD Locked in {{ tokenInfo.symbol }}</h3></div>
       </div>
+      <div class="mt-8 flex flex-col">
+        <button @click="toggleBuyModal" class="btn bg-purple-100 my-8 w-42 mx-auto">Buy {{ tokenInfo.symbol }}</button>
+        <div v-if="modalIsOpen" class="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-40 flex items-center justify-center">
+          <div class="bg-white border border-black p-10">
+            <div class="flex justify-between w-100">
+              Ether to Stake:
+              <button @click="ethToCheck -= .00001">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </button>
+              <input v-model="ethToCheck" @input="debounceListener" step=".00001" type="number" class="bg-transparent border-b-2 border-black">
+              <button @click="ethToCheck += .00001">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </button>
+            </div>
+            <button @click="checkEth" class="btn bg-purple-100 my-8 w-42 mx-auto">Calculate Price for {{ tokenInfo.symbol }}</button>
+            <div>
+              Amount of {{ tokenInfo.symbol }} to be received for {{ ethToCheck }} Eth
+              <br><strong>{{ amountToBeReceivedFromStakingEth }}</strong> {{ amountToBeReceivedFromStakingEth === '' ? '' : tokenInfo.symbol }}
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else-if="tokenStatus === 'FAIL'">
+      <h3 class="text-xl font-black">Token is not deployed yet. If you just deployed it, check the address on
+        EtherScan</h3>
+    </div>
   </div>
 </template>
 
@@ -38,7 +67,32 @@ export default defineComponent({
   props: ["username"],
   components: { ImageContainer },
   setup: (props) => {
+    const amountToBeReceivedFromStakingEth = ref<string>("")
+    const ethToCheck = ref<number>(0)
+    const modalIsOpen = ref<boolean>(false)
+    async function checkEth() {
+      const formdata = new FormData();
+      formdata.append("etherToCheck", String(Math.floor(ethToCheck.value * Math.pow(10, 18))));
+
+      const requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow"
+      } as RequestInit
+
+      const request = await fetch(import.meta.env.VITE_BACKEND_URL + "/api/v0/users/" + props.username + "/token/stakingrewards", requestOptions)
+      const response = await request.json() as { toBuyer: string, toOwner: string }
+      amountToBeReceivedFromStakingEth.value = response.toBuyer
+    }
+    function toggleBuyModal() {
+      modalIsOpen.value = !modalIsOpen.value
+    }
     return {
+      amountToBeReceivedFromStakingEth,
+      checkEth,
+      ethToCheck,
+      toggleBuyModal,
+      modalIsOpen,
       ...composeTokenInfo(props.username),
       ...composeUserInfo(props.username)
     }
