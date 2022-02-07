9a0c6c40e20dfe104162681a235827f695a23cee
Javier Gonzalez <javier.e.glz@gmail.com>
2022-02-07 17:00:26 -0600

    hotfix: fix broken style

--- src/views/TokenSuccess.vue
+++ src/views/TokenSuccess.vue
@@ -2,9 +2,9 @@
   <div class="font-black text-4xl text-center py-48">
     Congratulations, Your token is live!!
     <div class="text-center my-8">
-      <p class="btn bg-purple-200 text-lg">
+      <div class="text-lg max-w-xs mx-auto">
         Redirecting you to admin page...
-      </p>
+      </div>
     </div>
   </div>
 </template>
