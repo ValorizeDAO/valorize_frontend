2bdb98151c4c27543637fd53034da6049b93000d
Javier Gonzalez <javier.e.glz@gmail.com>
2022-02-08 14:20:37 -0600

    show token balance on screen

--- src/views/AirdropCreationPage.vue
+++ src/views/AirdropCreationPage.vue
@@ -1,22 +1,49 @@
 <template>
   <div class="px-16 pt-8 font-black">
-    {{ c(totalSupply) }} Available for Airdrop
-    <div class="text-center my-8">
-    </div>
+    {{ c(contractTokenBalance) }} ({{ tokenData.symbol }}) Available for Airdrop
+    <div class="text-center my-8"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
-
+import { defineComponent, onMounted, ref } from "vue";
 export default defineComponent({
   name: "Token Success",
-  props: ['state'],
-  setup: (props) => {
-    return {
-    };
-  },
 });
+</script>
+
+<script setup lang="ts">
+import { TimedMintTokenFactory } from "../contracts/TimedMintTokenFactory";
+import { SimpleTokenFactory } from "../contracts/SimpleTokenFactory";
+import currency from "currency.js";
+import { ethers } from "ethers";
+import type { SimpleToken } from "../contracts/SimpleToken";
+const props = defineProps<{
+  state: any;
+}>();
+
+const { state } = props;
+const { tokenData, tokenAdmins } = state;
+const contractTokenBalance = ref("");
+const token = ref<SimpleToken | null>(null);
+const ethereum: any = (window as any).ethereum;
+const { formatEther } = ethers.utils;
+
+onMounted(async () => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  token.value = new SimpleTokenFactory(signer).attach(tokenData.address);
+  const balance = await token.value.balanceOf(tokenData.address);
+  contractTokenBalance.value = formatEther(balance.toString());
+});
+
+function c(value: string) {
+  return currency(Number(value), {
+    separator: ",",
+    precision: 0,
+    symbol: "",
+  }).format();
+}
 </script>
 
 <style scoped>
