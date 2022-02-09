90020e5375820054577d747287e277b6c292c332
Javier Gonzalez <javier.e.glz@gmail.com>
2022-02-08 21:17:08 -0600

    Add node polyfill to use buffer for merkle root

--- src/views/AirdropCreationPage.vue
+++ src/views/AirdropCreationPage.vue
@@ -4,65 +4,82 @@
       {{ c(contractTokenBalance) }} ({{ tokenData.symbol }}) Available for
       Airdrop
     </div>
-    <div
-      v-if="airdropData.length <= 1"
-      id="drop-airdrop-csv"
-      class="md:px-16 mt-4"
-    >
-      <div class="border-black border-4 border-dashed bg-purple-100 mx-auto">
-        <div class="h-[20rem] flex flex-col justify-center items-center">
-          Add a CSV File
-          <button @click="triggerUploadForm" class="btn my-4">Upload</button>
-          <input accept=".csv" type="file" ref="uploadButton" class="sr-only" />
-        </div>
-      </div>
-    </div>
-    <div v-else class="md:mx-16 mt-4 w-100">
-      <div class="max-w-2xl mx-auto mb-8 bg-paper-lighter">
-        <div
-          class="
-            text-2xl
-            bg-paper-default
-            border-b-2 border-x-paper-dark
-            font-black
-            px-4
-            py-2
-            flex
-            justify-between
-          "
-        >
-          <h2>address</h2>
-          <h2>airdrop amount</h2>
-        </div>
-        <div class="max-h-[60vh] overflow-y-auto bg-paper-light">
-          <div
-            v-for="(data, index) in airdropData"
-            :key="index"
-            class="py-2 px-4 flex justify-between border-b-2 border-paper-dark"
-          >
-            <p class="mr-6">{{ data[0] }}</p>
-            <p>{{ c(data[1]) }}</p>
+    <transition name="fade" mode="out-in">
+      <div
+        v-if="airdropData.length <= 1"
+        id="drop-airdrop-csv"
+        class="md:px-16 mt-4"
+      >
+        <div class="border-black border-4 border-dashed bg-purple-100 mx-auto">
+          <div class="h-[20rem] flex flex-col justify-center items-center">
+            Add a CSV File
+            <button @click="triggerUploadForm" class="btn my-4">Upload</button>
+            <input
+              accept=".csv"
+              type="file"
+              ref="uploadButton"
+              class="sr-only"
+            />
           </div>
         </div>
-        <div
-          class="
-            text-2xl
-            bg-paper-default
-            border-b-2 border-x-paper-dark
-            font-black
-            px-4
-            py-2
-            flex
-            justify-between
-          "
-        >
-          <h2>Total to Airdrop</h2>
-          <h2>
-            {{ c(totalAirdropAmount) }}
-          </h2>
-        </div>
       </div>
-    </div>
+      <div v-else class="md:mx-16 mt-4 w-100">
+        <h2 class="text-3xl font-black">Please Verify Airdrop Ammounts</h2>
+        <div class="max-w-2xl mt-4 mx-auto mb-8 bg-paper-lighter">
+          <div
+            class="
+              text-2xl
+              bg-paper-default
+              border-b-2 border-x-paper-dark
+              font-black
+              px-4
+              py-2
+              flex
+              justify-between
+            "
+          >
+            <h3>Address</h3>
+            <h3>Airdrop amount</h3>
+          </div>
+          <div class="max-h-[40vh] overflow-y-auto bg-paper-light">
+            <div
+              v-for="(data, index) in airdropData"
+              :key="index"
+              class="
+                py-2
+                px-4
+                flex
+                justify-between
+                border-b-2 border-paper-dark
+              "
+            >
+              <p class="mr-6">{{ data[0] }}</p>
+              <p>{{ c(data[1]) }}</p>
+            </div>
+          </div>
+          <div
+            class="
+              text-2xl
+              bg-paper-default
+              border-b-2 border-b-paper-dark
+              font-black
+              px-4
+              py-2
+              flex
+              justify-between
+            "
+          >
+            <h2>Total to Airdrop</h2>
+            <h2>
+              {{ c(totalAirdropAmount) }}
+            </h2>
+          </div>
+        </div>
+        {{ merkleRoot }}
+        <button @click="getMerkleRootFromLeaves" class="">get root</button>
+        {{ merkleLeaves }}
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -77,8 +94,11 @@ export default defineComponent({
 import { TimedMintTokenFactory } from "../contracts/TimedMintTokenFactory";
 import { SimpleTokenFactory } from "../contracts/SimpleTokenFactory";
 import currency from "currency.js";
-import { ethers } from "ethers";
+import { ethers, BigNumber } from "ethers";
 import type { SimpleToken } from "../contracts/SimpleToken";
+import { MerkleTree } from "merkletreejs";
+import { keccak_256 } from "js-sha3";
+
 const props = defineProps<{
   state: any;
 }>();
@@ -91,6 +111,7 @@ const token = ref<SimpleToken | null>(null);
 const ethereum: any = (window as any).ethereum;
 const { formatEther } = ethers.utils;
 const csvDump = ref("");
+const merkleRoot = ref("");
 const airdropData = computed(() => {
   const csvSplit = csvDump.value.split("\r\n");
   const airdropTuple = csvSplit.map((item) => {
@@ -98,9 +119,24 @@ const airdropData = computed(() => {
   });
   return airdropTuple;
 });
+const merkleLeaves = computed(() => {
+  const leaves = airdropData.value.map((baseNode: string[]) => {
+    return ethers.utils.solidityKeccak256(
+      ["address", "uint256"],
+      [baseNode[0], BigNumber.from(baseNode[1])]
+    );
+  });
+  return leaves;
+});
+function getMerkleRootFromLeaves() {
+  const merkleTree = new MerkleTree(merkleLeaves.value, keccak_256, {
+    sort: true,
+  });
+  merkleRoot.value = merkleTree.getHexRoot();
+}
 const totalAirdropAmount = computed(() => {
   const csvSplit = csvDump.value.split("\r\n");
-  const amountsOnly = csvSplit.map((item, index) => {
+  const amountsOnly = csvSplit.map((item) => {
     return item.split(",")[1];
   });
   return amountsOnly.reduce((partialSum, a) => partialSum + parseInt(a), 0);
@@ -123,16 +159,6 @@ function triggerUploadForm() {
       fr.readAsText(e.target?.files[0]);
     });
   }
-}
-function upload(e: Event) {
-  console.log(e);
-  const inputElement = e.target as HTMLInputElement;
-  const files = inputElement.files;
-
-  const fileReader = new FileReader();
-  fileReader.addEventListener("load", (e) => {
-    console.log(e.target.files);
-  });
 }
 
 function c(value: string) {
