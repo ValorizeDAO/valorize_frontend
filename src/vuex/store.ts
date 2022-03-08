18b4ac37772435149e23be7d51113fe710b23a70
Javier Gonzalez <javier.e.glz@gmail.com>
2022-03-08 16:29:23 -0600

    Add LogRocket integration

--- src/vuex/store.ts
+++ src/vuex/store.ts
@@ -1,10 +1,14 @@
-import { createStore } from "vuex"
-import authUser from "./modules/authUserModule"
-import userToken from "./modules/userTokenModule"
+import { createStore } from "vuex";
+import authUser from "./modules/authUserModule";
+import userToken from "./modules/userTokenModule";
+import createPlugin from "logrocket-vuex";
+import LogRocket from "logrocket";
 
+const logrocketPlugin = createPlugin(LogRocket);
 export default createStore({
   modules: {
     authUser,
     userToken,
   },
-})
+  plugins: [logrocketPlugin],
+});
