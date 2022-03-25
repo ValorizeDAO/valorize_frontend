9c8653f320d329fede3f89f116b080abd6ac68c6
Javier Gonzalez <javier.e.glz@gmail.com>
2022-03-25 17:55:56 -0500

    Test alpha users

--- src/views/__tests__/EditProfilePage.spec.ts
+++ src/views/__tests__/EditProfilePage.spec.ts
@@ -1,29 +1,30 @@
 import { shallowMount, DOMWrapper } from "@vue/test-utils";
 import { mocked } from "ts-jest/utils";
 import EditProfilePage from "@/views/EditProfilePage.vue";
-import Vuex, { Store, createStore } from "vuex";
+import Vue, { useStore } from "vuex";
 import auth from "../../services/authentication";
 
 jest.mock("../../services/authentication", () => ({}));
 const mockedAuth = mocked(auth, true);
-jest.mock("vuex", () => ({
-  useStore: jest.fn(() => ({
-    state: {
-      authUser: {
-        user: {
-          id: 1,
-          name: "test",
-          email: "test@email.com",
-          about: "test",
-          isAlphaUser: false,
-        },
+const mockStore = {
+  state: {
+    authUser: {
+      user: {
+        id: 1,
+        name: "test",
+        email: "test@email.com",
+        about: "test",
+        isAlphaUser: false,
       },
     },
-    getters: {
-      "authUser/hasToken": jest.fn(() => true),
-      "authUser/profileImage": jest.fn(() => "fake_url.jpg"),
-    },
-  })),
+  },
+  getters: {
+    "authUser/hasToken": jest.fn(() => true),
+    "authUser/profileImage": jest.fn(() => "fake_url.jpg"),
+  },
+};
+jest.mock("vuex", () => ({
+  useStore: jest.fn(() => mockStore),
 }));
 
 describe("<EditProfilePage \\>", () => {
@@ -34,5 +35,22 @@ describe("<EditProfilePage \\>", () => {
   it("renders as expected", () => {
     const wrapper = shallowMount(EditProfilePage);
     expect(wrapper.html()).toMatchSnapshot();
+  });
+  describe("Alpha Users", () => {
+    it("Does not let a non-alpha user deploy a token", () => {
+      const wrapper = shallowMount(EditProfilePage);
+      const tokenLaunchComponent = wrapper.find("create-token-stub");
+      expect(tokenLaunchComponent.exists()).toBe(false);
+    });
+    it("lets alpha users see the <CreateToken \\> component", () => {
+      mockStore.state.authUser.user.isAlphaUser = true;
+      jest.mock("vuex", () => ({
+        useStore: jest.fn(() => mockStore),
+      }));
+      const wrapper = shallowMount(EditProfilePage);
+      console.log(wrapper.html());
+      const tokenLaunchComponent = wrapper.find("create-token-stub");
+      expect(tokenLaunchComponent.exists()).toBe(true);
+    });
   });
 });
