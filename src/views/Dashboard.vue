c2b7ee8b0a353b16ec95e4c58982bbd53683d0f8
Javier Gonzalez <javier.e.glz@gmail.com>
2022-03-28 16:07:20 -0500

    #98: udpate token dashboard page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,22 +1,39 @@
 
 <template>
   <div class="px-16 py-4">
-    <h1 class="text-5xl font-sans font-black">Welcome, {{ user.username }}!</h1>
-    <h2 class="text-3xl font-sans font-black my-4">
-      Get started by setting a profile and deploying your own token
-    </h2>
-    <router-link to="edit-profile" class="btn mx-auto my-3 w-40 bg-paper-light"
-      >Setup Profile</router-link
-    >
+    <div class="flex w-full justify-between">
+      <h1 class="text-3xl font-sans font-black">Welcome, {{ user.username }}!</h1>
+      <router-link to="edit-profile" class="btn  my-3 w-40 text-center bg-paper-light"
+        >
+        Setup Profile
+        </router-link>
+    </div>
+    <section id="manage-tokens">
+      <h2 class="text-2xl font-black my-4">Your Tokens</h2>
+      <div class="grid grid-cols-1 sm:grid-cols-3 gap-8">
+        <ImageContainer>
+        <div class="col-span-1 bg-purple-300 p-8 border-2 border-black">
+          <h3 class="text-lg font-black">Token Test</h3>
+        </div>
+        </ImageContainer> 
+        <ImageContainer>
+        <div class="col-span-1 bg-purple-300 p-8 border-2 border-black">
+          <h3 class="text-lg font-black">Token Test</h3>
+        </div>
+        </ImageContainer>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from "vue";
+import { defineComponent } from "vue";
 import { useStore } from "vuex";
+import ImageContainer from "../components/ImageContainer.vue";
 export default defineComponent({
   name: "Dashboard",
   props: {},
+  components: { ImageContainer },
   setup: () => {
     const store = useStore();
     const user = store.getters["authUser/user"];
