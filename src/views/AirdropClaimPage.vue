2ef0faf7095c117a8054a313bb929584b46aa373
Javier Gonzalez <javier.e.glz@gmail.com>
2022-04-09 12:15:32 -0500

    #55: execution reverted message

--- src/views/AirdropClaimPage.vue
+++ src/views/AirdropClaimPage.vue
@@ -1,86 +1,93 @@
 <template>
   <div>
-    {{ claimStatus }}
-    <div class="mx-auto w-[28rem]">
-      <div class="h-40 mt-12 flex flex-col justify-between">
-        <div class="opacity-0"></div>
-        <transition name="fade" mode="out-in">
-          <div v-if="['INIT', 'CHECKING_VALIDITY'].includes(claimStatus)">
-            <label for="address-input">
-              <h2
-                class="font-black text-2xl max-w-md"
-              >Check if your address is available for claiming this airdrop</h2>
-            </label>
-            <div :class="claimStatus === 'CHECKING_VALIDITY' || 'opacity-0'" class=" font-black transition duration-100">Checking . . .</div>
-          </div>
-          <div v-else-if="claimStatus === 'CLAIM_AVAILABLE'" class="text-center">
-            <span class="font-bold">{{ formatAddress(address) }}</span>
-            has {{ c(toDecimals(claimAmount)) }} {{ tokenData.symbol }} tokens available to claim!
-            <div class="text-center">
-              <button class="btn mt-4" id="send-claim" @click="sendClaim">Claim Now</button>
+    <transition name="fade" mode="out-in">
+      <div v-if="claimStatus === 'TRANSACTION_SENT'" class="mx-auto w-[28rem]">
+        <div class="mx-auto mt-4 text-center">
+          <h2 class="text-xl font-black">
+            Search an address to claim airdrop tokens
+          </h2>
+        </div>
+      </div>
+      <div v-else class="mx-auto w-[28rem]">
+        <div class="h-40 mt-12 flex flex-col justify-between">
+          <transition name="fade" mode="out-in">
+            <div v-if="['INIT', 'CHECKING_VALIDITY'].includes(claimStatus)">
+              <label for="address-input">
+                <h2
+                  class="font-black text-2xl max-w-md"
+                >Check if your address is available for claiming this airdrop</h2>
+              </label>
+              <div :class="claimStatus === 'CHECKING_VALIDITY' || 'opacity-0'" class=" font-black transition duration-100">Checking . . .</div>
             </div>
-          </div>
+            <div v-else-if="claimStatus === 'CLAIM_AVAILABLE'" class="text-center">
+              <span class="font-bold">{{ formatAddress(address) }}</span>
+              has {{ c(toDecimals(claimAmount)) }} {{ tokenData.symbol }} tokens available to claim!
+              <div class="text-center">
+                <button class="btn mt-4" id="send-claim" @click="sendClaim">Claim Now</button>
+              </div>
+            </div>
 
-        <div v-else-if="claimStatus === 'METAMASK_REQUESTED'" class="h-20 w-[28rem] mx-auto mt-8 text-center">
-          <h2
-            class="font-black text-xl max-w-md"
-            v-if="claimStatus === 'METAMASK_REQUESTED'"
-          >Please confirm your transaction on a web3 provider like Metamask</h2>
-        </div>
+          <div v-else-if="claimStatus === 'METAMASK_REQUESTED'" class="h-20 w-[28rem] mx-auto mt-8 text-center">
+            <h2
+              class="font-black text-xl max-w-md"
+              v-if="claimStatus === 'METAMASK_REQUESTED'"
+            >Please confirm your transaction on a web3 provider like Metamask</h2>
+          </div>
+            <div
+              v-else-if="claimStatus === 'ADDRESS_MISMATCH'"
+              id="transaction-addressMismatch"
+              class="mx-auto"
+            >
+              Address mismatch! you are currently connected to
+              <span
+                class="font-black"
+              >{{ formatAddress(userAddress) }}</span>,
+              <br />and cannot claim tokens allocated to
+              <br />
+              <span class="font-black">{{ address }}</span>.<br>
+              <br>
+              <h2 class="text-xl font-black">
+                Search a different address?
+              </h2>
+            </div>
+          </transition>
           <div
-            v-else-if="claimStatus === 'ADDRESS_MISMATCH'"
-            id="transaction-addressMismatch"
-            class="mx-auto"
+            class="mt-8 flex flex-wrap justify-between"
+            :class="!['INIT', 'ADDRESS_MISMATCH', 'CLAIM_UNAVAILABLE'].includes(claimStatus) ? 'opacity-50' : ''"
           >
-            Address mismatch! you are currently connected to
-            <span
-              class="font-black"
-            >{{ formatAddress(userAddress) }}</span>,
-            <br />and cannot claim tokens allocated to
-            <br />
-            <span class="font-black">{{ address }}</span>.<br>
-            <br>
-            <h2 class="text-xl font-black">
-              Search a different address?
-            </h2>
+            <input
+              class="bg-purple-100 border-b-2 border-black px-2 flex-grow"
+              type="text"
+              v-model="address"
+              id="address-input"
+            />
+            <button class="btn" @click="getAirdropClaimAmount" id="submit-button">search</button>
           </div>
-        </transition>
-        <div
-          class="mt-8 flex flex-wrap justify-between"
-          :class="!['INIT', 'ADDRESS_MISMATCH', 'CLAIM_UNAVAILABLE'].includes(claimStatus) ? 'opacity-50' : ''"
-        >
-          <input
-            class="bg-purple-100 border-b-2 border-black px-2 flex-grow"
-            type="text"
-            v-model="address"
-            id="address-input"
-          />
-          <button class="btn" @click="getAirdropClaimAmount" id="submit-button">search</button>
+          <div
+            v-if="claimStatus === 'CLAIM_UNAVAILABLE'"
+            id="search-error"
+            class="mx-auto mt-4 text-red-900 text-center font-black"
+          >
+          * Address is not avaliable for this airdrop
+          </div>
         </div>
         <div
-          v-if="claimStatus === 'CLAIM_UNAVAILABLE'"
-          id="search-error"
-          class="mx-auto mt-4 text-red-900 text-center font-black"
+          v-if="['METAMASK_REQUESTED', 'TX_PENDING', 'TX_SUCCESS', 'ERROR'].includes(claimStatus)"
+          id="claim-section"
         >
-        * Address is not avaliable for this airdrop
+          <transition name="fade">
+            <div id="transaction-executing" v-if="claimStatus === 'TX_PENDING'">Confirming Transaction</div>
+            <div id="transaction-error" v-else-if="claimStatus === 'ERROR'">{{ errorMessage }}</div>
+            <div id="transaction-success" v-else-if="claimStatus === 'TX_SUCCESS'">
+              SUCCESS!
+              <router-link
+                :to="{ path: '/register', query: { registerAddress: userAddress, redirectUri: currentRoute } }"
+              >Register this address to your Valorize Profile and get notified of new airdrops!</router-link>
+            </div>
+          </transition>
         </div>
       </div>
-      <div
-        v-if="['METAMASK_REQUESTED', 'TX_PENDING', 'TX_SUCCESS', 'ERROR'].includes(claimStatus)"
-        id="claim-section"
-      >
-        <transition name="fade">
-          <div id="transaction-executing" v-if="claimStatus === 'TX_PENDING'">Confirming Transaction</div>
-          <div id="transaction-error" v-else-if="claimStatus === 'ERROR'">{{ errorMessage }}</div>
-          <div id="transaction-success" v-else-if="claimStatus === 'TX_SUCCESS'">
-            SUCCESS!
-            <router-link
-              :to="{ path: '/register', query: { registerAddress: userAddress, redirectUri: currentRoute } }"
-            >Register this address to your Valorize Profile and get notified of new airdrops!</router-link>
-          </div>
-        </transition>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -175,8 +182,15 @@ export default defineComponent({
         claimStatus.value = statuses[8] // TX_SUCCESS
       } catch (err: any) {
         claimStatus.value = statuses.at(-1) as string // ERROR
-        console.error(err)
-        errorMessage.value = "There was an error claiming your tokens. Please try again later."
+        console.error(err.code)
+        if (err.code === "UNPREDICTABLE_GAS_LIMIT") {
+          console.log(err.message)
+          const errorBody = JSON.parse("{" + err.message.split("{")[1]
+            .split("}")[0] + "}").message
+          errorMessage.value = errorBody || "Transaction failed. Please try again."
+        } else {
+          errorMessage.value = "There was an error claiming your tokens. Please try again later."
+        }
       }
     }
     function addEventListenerToAddressInput() {
